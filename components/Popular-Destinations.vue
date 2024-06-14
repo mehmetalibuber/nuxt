88f30a9bffa969ed09ba3268d8fd0d117d5fbd26
mehmetalibuber <mehmetalibuber6@gmail.com>
2024-06-15 00:17:56 +0300

    .

--- components/Popular-Destinations.vue
+++ components/Popular-Destinations.vue
@@ -84,7 +84,7 @@
 <script setup>
 import { VueperSlides, VueperSlide } from "vueperslides";
 import "vueperslides/dist/vueperslides.css";
-//import config from "~/config.js";
+//import config from "@/config.js";
 const Destinations = ref([]);
 const ImageFilePrefix = ref("");
 const localPath = useLocalePath();
