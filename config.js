464c7f9fc675d485092db514b7f3459d712539af
mehmetalibuber <mehmetalibuber6@gmail.com>
2024-06-21 11:26:56 +0300

    s

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  CRM_HOST: "https://testcrm.geccele.com/api/web",
+  //CRM_HOST: "https://crm.geccele.com/api/web",
+  CRM_WEBSITE: "1",
+  GoogleMapsAPIKey: "AIzaSyB1jnI1NLP_vSzfjXBGYAUb9R99s0S4rVE",
+  CRM_FILTER_URL: "",
+  CRM_TURKIYE_ID: "225",
+  CRM_ANTALYA_ID: "2169",
+  CRM_FILTER_NEIGHBORHOOD: "neighborhood",
+  CRM_PAYMENT_TYPE: "1",
+  GOOGLE_DIRECTION_URL: "https://www.google.com/maps/dir/?api=1&destination=",
+};
+export default config;
